cd7714119d04c1a460978a709958a74862f9591c
bhanusaia <bhanusai.adabala@gmail.com>
2020-01-31 17:23:46 +0000

    mvn commit

---
+++ src/main/java/com/medical-store/com/App.java
@@ -0,0 +1,13 @@
+package com.medical-store.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
